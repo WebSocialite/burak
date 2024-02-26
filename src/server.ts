2683aa7ba33fd5c933299914b7cfedbf84c5683d
sherboev boburmirzo <sherboevboburmirzo@sherboevs-MacBook-Air.local>
2024-02-26 15:13:56 +0500

    feat: updated

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-import dotenv from "dotenv";
+const dotenv =require ("dotenv");
 dotenv.config();
 
-import mongoose from 'mongoose';
+const mongoose =require ('mongoose');
 import app from "./app";
 
 mongoose
 .connect(process.env.MONGO_URL as string, {})
 
-.then((data) => {
+.then((data: any) => {
     console.log("MongoDB succeed");
     const PORT = process.env.PORT ?? 3003;
     app.listen(PORT, function () {
@@ -15,4 +15,4 @@ mongoose
     })
     
 })
-.catch((err) => console.log("ERRON ON connection mongodb:", err));
+.catch((err: any) => console.log("ERRON ON connection mongodb:", err));
