6fbc7a8d7164cdf8bc4e1eff83b64f70a96de0fb
sherboev boburmirzo <sherboevboburmirzo@sherboevs-MacBook-Air.local>
2024-02-26 07:45:06 +0500

    feat: express integration and mongoose connection

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config();
-console.log("PORT:", process.env.PORT);
-console.log("MONGO_URL:", process.env.MONGO_URL);
+
+import mongoose from 'mongoose';
+import app from "./app";
+
+mongoose
+.connect(process.env.MONGO_URL as string, {})
+
+.then((data) => {
+    console.log("MongoDB succeed");
+    const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function () {
+        console.log(`The server is running successfully: ${PORT}`);
+    })
+    
+})
+.catch((err) => console.log("ERRON ON connection mongodb:", err));
