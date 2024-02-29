a058972f883d02938313b29a62e73ffa3d30aef9
sherboev boburmirzo <sherboevboburmirzo@sherboevs-MacBook-Air.local>
2024-02-29 12:05:19 +0500

    Feat: create project standards

--- src/train.ts
+++ src/train.ts
@@ -1,9 +1,23 @@
-function getPositive(arr: number[]) {
-    const positiveValues = arr.filter(num => num > 0);
-    const result = positiveValues.join('');
+/* Project Standards: 
+-Logging standards
+-Naming standards
+Function, method, variable => CAMEL case
+class => PASCAL case
+folder => KEBAB case
+css => SNAKE case
+*/
 
-    return result;
-}
-const inputArray = [1, -4, 2];
-const result = getPositive(inputArray);
-console.log(result); // Output: "12"
+
+
+
+
+           // TASK H
+// function getPositive(arr: number[]) {
+//     const positiveValues = arr.filter(num => num > 0);
+//     const result = positiveValues.join('');
+
+//     return result;
+// }
+// const inputArray = [1, -4, 2];
+// const result = getPositive(inputArray);
+// console.log(result); // Output: "12"
