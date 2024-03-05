a6131c7fadf3a5432e91732d20ad3c948c87e82e
WebSocialite <106961349+WebSocialite@users.noreply.github.com>
2024-03-05 13:38:13 +0900

    feat: MIT TASK I

--- src/train.ts
+++ src/train.ts
@@ -11,13 +11,34 @@ css => SNAKE case
 
 
 
-        //    TASK H
-function getPositive(arr: number[]) {
-    const positiveValues = arr.filter(num => num > 0);
-    const result = positiveValues.join('');
+        //    TASK P
+        //    TASK O
+        //    TASK N
+        //    TASK M
+        //    TASK L
+        //    TASK K
+        //    TASK J
+        //    TASK I
+        function getDigits(inputString: string) {
+            const digitsArray = inputString.match(/\d/g);
+            
+            return digitsArray ? digitsArray.join('') : '';
+        }
+        const inputString = "m14i1t";
+        const result = getDigits(inputString);
+        console.log(result);
 
-    return result;
-}
-const inputArray = [1, -4, 2];
-const result = getPositive(inputArray);
-console.log(result); // Output: "12"
+
+
+
+
+//         //    TASK H
+// function getPositive(arr: number[]) {
+//     const positiveValues = arr.filter(num => num > 0);
+//     const result = positiveValues.join('');
+
+//     return result;
+// }
+// const inputArray = [1, -4, 2];
+// const result = getPositive(inputArray);
+// console.log(result); // Output: "12"
