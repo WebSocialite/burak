3f7832be56fe5574628ffef3b758e0cc5af9b294
sherboev boburmirzo <sherboevboburmirzo@sherboevs-MacBook-Air.local>
2024-03-18 22:36:13 +0500

    feat:develop FrontEnd signup process

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,6 +1,30 @@
 console.log("Signup frontend javascript file");
 
-$(function(){ });
+$(function () {
+    const fileTarget = $(".file-box .upload-hidden"); 
+    let filename; 
+
+    fileTarget.on("change", function () {
+        if (window.FileReader) {
+            const uploadFile = $(this)[0].files[0];
+            console.log("uploadFile:", uploadFile);
+            const fileType = uploadFile["type"];
+            const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+            if(validImageType.includes(fileType)) {
+                alert("please insert only jpg, jpeg and png!")
+            } else {
+                if (uploadFile) {
+                    console.log(URL.createObjectURL(uploadFile));
+                    $(".upload-img-frame")
+                    .attr("src", URL.createObjectURL(uploadFile))
+                    .addClass("success");
+                }
+                filename = $(this)[0].files[0].name;
+            }
+            $(this).siblings(".upload-name").val(filename);
+        }
+    });
+ });
 
 function validateSignupForm() {
     const memberNick = $(".member-nick").val();
@@ -8,7 +32,8 @@ function validateSignupForm() {
     const memberPassword = $(".member-password").val();
     const confirmPassword = $(".confirm-password").val();
 
-    if(memberNick === "" || 
+    if(
+    memberNick === "" || 
     memberPhone === "" || 
     memberPassword === "" || 
     confirmPassword === ""
@@ -21,5 +46,12 @@ function validateSignupForm() {
         alert("Password differs, please check!"); 
         return false;
     }
+   const memberImage = $(".member-image").get(0).files[0] ? $(".member-image")
+   .get(0).files[0].name
+: null;
+if(!memberImage) {
+    alert("please insert restaurant image!");
+    return false;
+}
 
 }
