2e322be62ffaf4e0090d265c8ab5159b48b07c64
sherboev boburmirzo <sherboevboburmirzo@sherboevs-MacBook-Air.local>
2024-03-19 18:30:34 +0500

    fix: modify spa logic rest api logic

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -11,6 +11,20 @@ $(function () {
        // TODO: Axios updateChosenUser
        axios.post("/admin/user/edit", {
         _id: id,
-       "memberStatus": memberStatus});
+       "memberStatus": memberStatus
+    }).then((response) => {
+        console.log('response:', response);
+        const result = response.data;
+        console.log('result:', result);
+
+        if(result.data) {
+            console.log("user updated!");
+            $(".member-status").blur();
+        } else alert("user update failed!");
+    })
+    .catch((err) => {
+        console.log(err);
+        alert("user update failed!");
+    });
     });
 });
