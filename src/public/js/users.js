83d034bca208b831aaad45bab7ad7bc9bf204087
sherboev boburmirzo <sherboevboburmirzo@sherboevs-MacBook-Air.local>
2024-03-19 18:13:12 +0500

    feat: frontend updateChosenUser logic

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -1,1 +1,16 @@
 console.log("Users frontend javascript file");
+
+$(function () {
+    $(".member-status").on("change", function(e) {
+        const id = e.target.id;
+        console.log("id:", id);
+
+        const memberStatus = $(`#${id}.member-status`).val();
+        console.log('memberStatus:', memberStatus);
+
+       // TODO: Axios updateChosenUser
+       axios.post("/admin/user/edit", {
+        _id: id,
+       "memberStatus": memberStatus});
+    });
+});
