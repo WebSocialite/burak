d332d5ab8ff9c1a3c574e7810ccf397b4b038202
sherboev boburmirzo <sherboevboburmirzo@sherboevs-MacBook-Air.local>
2024-04-12 08:44:32 +0500

    feat: create order related schema model

---
+++ src/schema/OrderItem.model.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from 'mongoose';
+
+
+const orderItemSchema = new Schema({
+
+    ItemQuantity: {
+        type: Number,
+        required: true,
+    },
+    itemPrice: {
+        type: Number,
+        required: true,
+    },
+    orderId: {
+        type: Schema.Types.ObjectId,
+        ref: "Order",
+    },
+    productId: {
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+    },
+},
+ { timestamps: true, 
+    collection: "orderItems" } //bu yerda "collection" orqali default bolip turgan OrderItem ni orderItem ga ozgartishini beryapmiz
+);
+
+export default mongoose.model("OrderItem", orderItemSchema );
